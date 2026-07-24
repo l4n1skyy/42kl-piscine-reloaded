f26b84a2c48d87753c5f79f8945098bde88b0e13
Lanisha <lanishaanusri28@gmail.com>
2026-07-24 09:54:34 +0800

    .

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -7,19 +7,33 @@ char *ft_strdup(char *src)
 {
 	char *read;
 	char *write;
+	char *dest;
 	int len;
 
+	if (!src)
+		return (NULL);
 	len = ft_strlen(src);
-
-	if (!len)
-		return NULL;
+	dest = malloc(len + 1);
+	if (!dest)
+		return (NULL);
+	read = src;
+	write = dest;
+	while (*read)
+	{
+		*write = *read;
+		write++;
+		read++;
+	}
+	*write = '\0';
+	return (dest);
 }
 
 int ft_strlen(char *str)
 {
 	int len;
+
 	len = 0;
 	while (*str++)
 		len++;
-	return len;
+	return (len);
 }
