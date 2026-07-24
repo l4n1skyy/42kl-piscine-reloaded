8c54e68c3855566cfc4e1494a34fde7215ee88b0
Lanisha <lanishaanusri28@gmail.com>
2026-07-24 21:01:26 +0800

    .

--- _SUBMISSIONS/recode01/ex02/ft_putnbr.c
+++ _SUBMISSIONS/recode01/ex02/ft_putnbr.c
@@ -12,7 +12,7 @@
 
 #include <unistd.h>
 
-void	ft_putnbr(int nb);
+void ft_putnbr(int nb);
 /*
 int	main(void)
 {
@@ -22,24 +22,21 @@ int	main(void)
 	ft_putnbr(n);
 }
 */
-	// INT_MIN
-	// negatives
-	// base case
-void	ft_putnbr(int nb)
+void ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
-		return ;
-		}
+		return;
+	}
 	if (nb < 0)
 	{
 		write(1, "-", 1);
 		nb = -nb;
-		}
+	}
 	if (nb >= 10)
 		ft_putnbr(nb / 10);
-	char	c;
+	char c;
 	c = nb % 10 + '0';
 	write(1, &c, 1);
 }
